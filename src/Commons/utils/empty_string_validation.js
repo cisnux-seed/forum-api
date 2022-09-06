c7ab8729576b6a96a7ee2f034a7a6ca8ce83062e
cisnux-seed <fajrarisqulla@gmail.com>
2022-09-07 02:07:10 +0700

    updated .gitignore

--- src/Commons/utils/empty_string_validation.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-extend-native */
-Object.defineProperty(String.prototype, 'isEmpty', {
-  enumerable: false,
-  get() {
-    return this.trim().length === 0;
-  },
-});
--- src/Commons/utils/undefined_variable_validation.js
+++
@@ -1,5 +0,0 @@
-// to avoid program throw an incomplete properties error,
-// when properties are complete but there is a property equal to false
-const isUndefined = (item) => typeof item === 'undefined' || item === null;
-
-module.exports = isUndefined;
